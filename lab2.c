f470fa46e6a9e85ad0a10f34292ac1f32320107f
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-20 18:15:59 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -40,8 +40,8 @@ pthread_t network_thread;
 void *network_thread_f(void *);
 
  const struct KeyState alphabet[] = {
-	    {0x00, {0x04, 0x00}, ''A''},  // Example entry 1
-	    {0x02, {0x21, 0x22}, ''B''} // Example entry 2
+	    {0x00, {0x04, 0x00}, 'A'},  // Example entry 1
+	    {0x02, {0x21, 0x22}, 'B'} // Example entry 2
 	    /*{0x03, {0x31, 0x32}, 'C'},*/  // Example entry 3
 	    // Add more entries as needed
 	};
