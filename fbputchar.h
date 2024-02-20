587f7039f843a759a08230be360ba955eab0dda5
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-20 17:16:39 -0500

    Update fbputchar.h

--- fbputchar.h
+++ fbputchar.h
@@ -7,16 +7,17 @@
 #define FBOPEN_MMAP -4         /* Couldn't mmap the framebuffer memory */
 #define FBOPEN_BPP -5          /* Unexpected bits-per-pixel */
 
-extern int fbopen(void);
-extern void fbputchar(char, int, int);
-extern void fbputs(const char * , int, int);
-extern void fbclean(int,int);
-extern char findWord(const struct KeyState, unsigned char, unsigned char, unsigned char);
-
 typedef struct KeyState {
     unsigned char modifiers;
     unsigned char keycode[2];
     char character;
     // Add other fields if needed
 };
+
+
+extern int fbopen(void);
+extern void fbputchar(char, int, int);
+extern void fbputs(const char * , int, int);
+extern void fbclean(int,int);
+extern char findWord(const struct KeyState, unsigned char, unsigned char, unsigned char);
 #endif
