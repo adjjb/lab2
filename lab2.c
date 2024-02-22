4342fd3e67635ea8a8e9efffa8da38199ade8fae
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-22 16:35:41 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -151,7 +151,7 @@ int main()
       }
       if (packet.keycode[0] == 0x2a){  /*backspace*/
       	int s = strlen(word); 
-	word[s-1] = ' ';
+	word[s-1] = '\0';
 	order = s-1;
 	fbclean(23,64,22,0);
 	printf("%s\n", word);
