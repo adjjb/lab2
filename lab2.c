a4bcbd5ac51c28313a97a735c3089e7b7e0b0372
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-22 15:25:08 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -139,7 +139,7 @@ int main()
 
       for (int i = 0; i <  sizeof(ascii_to_hid_key_map)/sizeof(ascii_to_hid_key_map[0]); ++i) {
         if (ascii_to_hid_key_map[i][0] == a && ascii_to_hid_key_map[i][1] == b ){
-		int record = ascii_to_hid_key_map[i][2]
+		int record = ascii_to_hid_key_map[i][2];
 	    	char newWord = (char) record;
             	word = strcat(word, newWord);
 	}
