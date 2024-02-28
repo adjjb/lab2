8b0bdc36e8123638c57b861d5649dafd6b0a249c
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-28 17:21:18 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -150,7 +150,7 @@ int main()
       if (b!= 0 ){
 	      for (int i = 0; i < strlen(word); ++i) {
         	smallWord[i-64*changeLine] = word[i];
-		fbputs(smallWord,21+changeLine , 0)
+		fbputs(smallWord,21+changeLine , 0);
 		if (i > (changeLine+1)*64){
 			changeLine ++;
 		      	for (int j = 0; j < 64; ++i) {
