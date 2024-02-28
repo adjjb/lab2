3fdc1b9a5b837511f63d9c33da98136be6d2d16c
adjjb <95053325+adjjb@users.noreply.github.com>
2024-02-28 17:51:53 -0500

    Update fbputchar.c

--- fbputchar.c
+++ fbputchar.c
@@ -117,7 +117,19 @@ void fbputchar(char c, int row, int col)
 void fbputs(const char *s, int row, int col)
 {
   char c;
-  while ((c = *s++) != 0) fbputchar(c, row, col++);
+  int count = 0;
+  int changLine = 0;
+  while (*s != 0) {
+    char c = *s;
+    fbputchar(c, row + changLine, col++);
+    s++;
+    count++;
+    if (count > 63){
+    	changLine ++; 
+	col = 0;
+	count = 0;
+    }
+}
 }
 
 
